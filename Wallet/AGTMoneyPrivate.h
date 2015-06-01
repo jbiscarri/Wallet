cbc9dc24329e97a73b5a55e6a570b08f49288856
Joan Biscarri <joan@biscarri.com>
2015-06-02 00:19:01 +0200

    AGTMoney clase abstracta

---
+++ Wallet/AGTMoneyPrivate.h
@@ -0,0 +1,13 @@
+//
+//  AGTMoneyPrivate.h
+//  Wallet
+//
+//  Created by Joan on 01/06/15.
+//  Copyright (c) 2015 Biscarri. All rights reserved.
+//
+
+#ifndef Wallet_AGTMoneyPrivate_h
+#define Wallet_AGTMoneyPrivate_h
+
+
+#endif
